96e893b30109d3574d604af8fb79afb9f3ab2267
Antonis Geralis <b3lievem@yandex.com>
2026-07-02 10:14:37 +0300

    review

--- tests/nim-style-guide_verification/test_c15_dotdot_spacing.nim
+++ tests/nim-style-guide_verification/test_c15_dotdot_spacing.nim
@@ -1,4 +1,4 @@
-## C15: Prefer spaces around .. and ..< in slices, loops, and range expressions.
+## C15: Write range operators compactly unless an operand contains an operator.
 ## Range type constructors use .., not ..<: range[0 ..< n] does not compile; range[0 .. n-1] does.
 
 import std/[assertions]
@@ -9,19 +9,19 @@ block range_with_spaces:
   var x: A = 3
   doAssert x == 3
 
-block slice_with_spaces:
+block compact_slices:
   let s = "abcdef"
-  doAssert s[0 .. 2] == "abc"
-  doAssert s[0 ..< 3] == "abc"
+  doAssert s[0..2] == "abc"
+  doAssert s[0..<3] == "abc"
 
 block dotdot_in_for:
   var total = 0
-  for i in 0 .. 4:
+  for i in 0..4:
     total += i
   doAssert total == 10
 
   var total2 = 0
-  for i in 0 ..< 5:
+  for i in 0..<5:
     total2 += i
   doAssert total2 == 10
 
